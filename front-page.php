c1e7b5cfb8320e2959a899b0ccc58cfe35797d02
Nicholas Romero <ncrmro@gmail.com>
2016-12-01 15:31:16 -0600

    Ands adds static images to front page

--- front-page.php
+++ front-page.php
@@ -4,14 +4,17 @@
  *
  * @package Floral_Events_Theme
  */
-if ('posts' == get_option('show_on_front')) : get_template_part('index'); else : get_header(); ?>
+if ('posts' == get_option('show_on_front')) : get_template_part('index');
+else : get_header(); ?>
     <div id="primary" class="no-sidebar content-area">
         <main id="main" class="no-sidebar site-main" role="main">
-            <?php while (have_posts()) : the_post();
 
-                the_content();
-
-            endwhile; ?>
+            <div class="front-page-gallery">
+                <img src="wp-content/themes/fe_theme/assets/images/front-page/aisle-decor.jpg">
+                <img src="wp-content/themes/fe_theme/assets/images/front-page/centerpiece.jpg">
+                <img src="wp-content/themes/fe_theme/assets/images/front-page/entry-display.jpg">
+                <img src="wp-content/themes/fe_theme/assets/images/front-page/flower-wall.jpg">
+            </div>
         </main>
     </div>
     <?php get_footer(); ?>
