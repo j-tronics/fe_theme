54753f8f3c883b9d2171ce1fd71ae00b2ca0c9e3
Nicholas Romero <ncrmro@gmail.com>
2016-12-07 17:26:21 -0600

    Adds custom ceremonies pages

---
+++ page-ceremonies.php
@@ -0,0 +1,16 @@
+<?php
+
+
+get_header(); ?>
+    <div id="primary" class="no-sidebar content-area">
+        <main id="main" class="no-sidebar site-main" role="main">
+            <div class="ceremonies_page">
+                <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/aisles.jpg">
+                <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/bouquets.jpg">
+                <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/personal_flowers.jpg">
+                <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/altars.jpg">
+                
+            </div>
+        </main><!-- #main -->
+    </div><!-- #primary -->
+<?php get_footer(); ?>
