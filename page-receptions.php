b2c82358e42b87d8937202582f7df748209e4b30
Nicholas Romero <ncrmro@gmail.com>
2016-12-09 14:03:56 -0600

    Adds sample receptions page

---
+++ page-receptions.php
@@ -0,0 +1,24 @@
+<?php
+
+
+get_header(); ?>
+    <div id="primary" class="no-sidebar content-area">
+        <main id="main" class="no-sidebar site-main" role="main">
+            <div class="ceremonies_page">
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/aisles.jpg">
+                </div>
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/bouquets.jpg">
+                </div>
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/personal_flowers.jpg">
+                </div>
+                <div>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/altars.jpg">
+                </div>
+
+            </div>
+        </main><!-- #main -->
+    </div><!-- #primary -->
+<?php get_footer(); ?>
