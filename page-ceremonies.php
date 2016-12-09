d49b2e50153428d5d01ae7d3e4aa1f9caedefca2
Nicholas Romero <ncrmro@gmail.com>
2016-12-09 09:28:43 -0600

    Fixes ceremonies page gallery css

--- page-ceremonies.php
+++ page-ceremonies.php
@@ -5,11 +5,19 @@ get_header(); ?>
     <div id="primary" class="no-sidebar content-area">
         <main id="main" class="no-sidebar site-main" role="main">
             <div class="ceremonies_page">
-                <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/aisles.jpg">
-                <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/bouquets.jpg">
-                <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/personal_flowers.jpg">
-                <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/altars.jpg">
-                
+                <div>
+                    <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/aisles.jpg">
+                </div>
+                <div>
+                    <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/bouquets.jpg">
+                </div>
+                <div>
+                    <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/personal_flowers.jpg">
+                </div>
+                <div>
+                    <img src="wp-content/themes/fe_theme/assets/images/ceremonies/index/altars.jpg">
+                </div>
+
             </div>
         </main><!-- #main -->
     </div><!-- #primary -->
