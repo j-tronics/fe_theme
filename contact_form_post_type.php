0f75d914bc393d774e908f1c85a4394e2fbd8c55
Nicholas Romero <ncrmro@gmail.com>
2016-12-15 11:35:28 -0600

    Creates custom contact form post type

---
+++ contact_form_post_type.php
@@ -0,0 +1,17 @@
+<?php
+function contact_form_submission() {
+    $labels = array(
+        'name'               => _x( 'Form Submissions', 'post type general name' ),
+        'singular_name'      => _x( 'Contact Form Submission', 'post type singular name' ),
+        'menu_name'          => 'Contact Form Submissions'
+    );
+    $args = array(
+        'labels'        => $labels,
+        'description'   => 'Holds our users contact form submissions',
+        'public'        => true,
+        'menu_position' => 5,
+        'supports'      => array( 'title', 'editor'),
+        'has_archive'   => true,
+    );
+    register_post_type( 'portfolio_item', $args ); }
+    add_action( 'init', 'contact_form_submission' );
