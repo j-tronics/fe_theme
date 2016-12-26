215bfb7b620940b61bb7483cc708a87cacbd76c8
Nicholas Romero <ncrmro@gmail.com>
2016-12-26 14:11:04 -0600

    Cleans up project dir

--- assets/js/expand-top-menu.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    // If the top men is not expanded
-    if ($('top-menu').attr('aria-expanded') === false) {
-        // Toggle the top-menu-expanded css class 
-        $( "ul#top-menu" ).toggleClass( "top-menu-expanded" )
-    }
-});
--- components/navigation/navigation-top.php
+++ components/navigation/navigation-top.php
@@ -4,15 +4,6 @@
 	<button class="menu-toggle" aria-controls="top-menu" aria-expanded="false">
 		<?php esc_html_e( 'Menu', 'fe_theme' ); ?>
 	</button>
-	<script>
-		$(function () {
-			// If the top men is not expanded
-			if ($('top-menu').attr('aria-expanded') === false) {
-				// Toggle the top-menu-expanded css class
-				$( "ul#top-menu" ).toggleClass( "top-menu-expanded" )
-			}
-		});
-	</script>
 
 	<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'top-menu' ) ); ?>
 
