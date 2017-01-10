0f9f8c08e3bef98c808719120ab5abcb745eb139
Nicholas Romero <ncrmro@gmail.com>
2017-01-10 09:25:53 -0600

    Adds automatic schedule form password generation

--- assets/js/schedule-form.js
+++ assets/js/schedule-form.js
@@ -1,4 +1,6 @@
-var password = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
-document.getElementById('birs_client_password').value = password;
-document.getElementById('birs_client_password_retype').value = password;
-
+var element = document.getElementById('birs_client_password');
+if (typeof(element) != 'undefined' && element != null) {
+    var password = Math.random().toString(36).slice(2) + Math.random().toString(36).slice(2);
+    document.getElementById('birs_client_password').value = password;
+    document.getElementById('birs_client_password_retype').value = password;
+}
