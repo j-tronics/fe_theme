23b9178f6597383ecd7c40166c0ed8f18102b47c
Nicholas Romero <ncrmro@gmail.com>
2017-01-10 09:26:46 -0600

    Updates site footer

--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,3 +1,3 @@
 <div class="site-info">
-	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'fe_theme' ), 'Floral Events Theme', '<a href="https://j-tronics.co" rel="designer">J-Tronics.co</a>' ); ?>
+	<?php printf(esc_html__('%1$s by %2$s.', 'fe_theme'), 'Site', '<a href="https://j-tronics.co" rel="designer">J-Tronics.co</a>'); ?>
 </div><!-- .site-info -->
