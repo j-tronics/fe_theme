9542a7461a859c1a353c0b3c92c47561f9b9b8ed
Nicholas Romero <ncrmro@gmail.com>
2017-01-10 09:26:18 -0600

    Updates the cermonies and receptions page

--- page-ceremonies.php
+++ page-ceremonies.php
@@ -4,20 +4,29 @@
 get_header(); ?>
     <div id="primary" class="no-sidebar content-area">
         <main id="main" class="no-sidebar site-main" role="main">
-            <div class="ceremonies_page">
-                <div>
+            <h1> Ceremonies </h1>
+            <div class="gallery_index_page">
+                <a href="<?php echo get_permalink(get_page_by_title('aisles')) ?>" class="gallery_column">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/aisles.jpg">
+                    <p>Aisles</p>
+                </a>
+                <div class="gallery_center">
+                    <a class="gallery_center_left" href="<?php echo get_permalink(get_page_by_title('bouquets')) ?>">
+                        <img
+                            src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/bouquets.jpg">
+                        <p>Bouquets</p>
+                    </a>
+                    <a class="gallery_center_right"
+                       href="<?php echo get_permalink(get_page_by_title('personal_flowers')) ?>">
+                        <img
+                            src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/personal_flowers.jpg">
+                        <p>Personal Flower</p>
+                    </a>
                 </div>
-                <div>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/bouquets.jpg">
-                </div>
-                <div>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/personal_flowers.jpg">
-                </div>
-                <div>
+                <a class="gallery_column" href="<?php echo get_permalink(get_page_by_title('altars')) ?>">
                     <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/altars.jpg">
-                </div>
-
+                    <p>Altars</p>
+                </a>
             </div>
         </main><!-- #main -->
     </div><!-- #primary -->
--- page-receptions.php
+++ page-receptions.php
@@ -2,23 +2,31 @@
 
 
 get_header(); ?>
-    <div id="primary" class="no-sidebar content-area">
-        <main id="main" class="no-sidebar site-main" role="main">
-            <div class="ceremonies_page">
-                <div>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/aisles.jpg">
-                </div>
-                <div>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/bouquets.jpg">
-                </div>
-                <div>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/personal_flowers.jpg">
-                </div>
-                <div>
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/ceremonies/index/altars.jpg">
-                </div>
-
+<div id="primary" class="no-sidebar content-area">
+    <main id="main" class="no-sidebar site-main" role="main">
+        <h1> Ceremonies </h1>
+        <div class="gallery_index_page">
+            <a href="<?php echo get_permalink(get_page_by_title('cakes')) ?>" class="gallery_column">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/receptions/index/cakes.jpg">
+                <p>Cakes</p>
+            </a>
+            <div class="gallery_center">
+                <a class="gallery_center_left" href="<?php echo get_permalink(get_page_by_title('entry-displays')) ?>">
+                    <img
+                        src="<?php echo get_template_directory_uri(); ?>/assets/images/receptions/index/entry-displays.jpg">
+                    <p>Entry displays</p>
+                </a>
+                <a class="gallery_center_right" href="<?php echo get_permalink(get_page_by_title('guest-tables')) ?>">
+                    <img
+                        src="<?php echo get_template_directory_uri(); ?>/assets/images/receptions/index/guest-tables.jpg">
+                    <p>Guest Tables</p>
+                </a>
             </div>
-        </main><!-- #main -->
-    </div><!-- #primary -->
+            <a class="gallery_column" href="<?php echo get_permalink(get_page_by_title('royal-table')) ?>">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/receptions/index/royal-table.jpg">
+                <p>Royal Table</p>
+            </a>
+        </div>
+    </main><!-- #main -->
+</div><!-- #primary -->
 <?php get_footer(); ?>
