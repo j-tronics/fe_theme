05dcff0b4fcc146ea169ddfb6e0409b7b215ad8d
Nicholas Romero <ncrmro@gmail.com>
2017-01-10 11:52:58 -0600

    Adds to site footer and removes images from .gitignore

--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,3 +1,3 @@
 <div class="site-info">
-	<?php printf(esc_html__('%1$s by %2$s.', 'fe_theme'), 'Site', '<a href="https://j-tronics.co" rel="designer">J-Tronics.co</a>'); ?>
+	<?php printf(esc_html__('Bill@floralevents.com | 4901 Milwee, #202 Houston, Texas 77092 | 713-863-7900 | By Appointment Only | %1$s by %2$s.', 'fe_theme'), 'Site', '<a href="https://j-tronics.co" rel="designer">J-Tronics.co</a>'); ?>
 </div><!-- .site-info -->
