a52403a772721bfa0a4e3b61fdeefb0aa8108e83
Nicholas Romero <ncrmro@gmail.com>
2017-01-31 13:50:10 -0600

    Adds random choice between consultant choices

--- assets/js/schedule-form.js
+++ assets/js/schedule-form.js
@@ -4,3 +4,21 @@ if (typeof(element) != 'undefined' && element != null) {
     document.getElementById('birs_client_password').value = password;
     document.getElementById('birs_client_password_retype').value = password;
 }
+
+
+window.onload = function () {
+    var chosenValue = Math.random() < 0.5 ? "Erin R" : "Javier";
+
+
+    var sel = document.getElementById('birs_appointment_staff');
+    var opts = sel.options;
+
+
+    for (var opt, j = 0; opt = opts[j]; j++) {
+        console.log(opt);
+        if (opt.text == chosenValue) {
+            sel.selectedIndex = j;
+        }
+    }
+
+};
